ea5ae6d892760dcec6f3e890ef2cb80acdfa6058
Devair Restani <devair.juca2013@gmail.com>
2020-11-25 23:33:56 -0300

    rota atualização usuario

--- src/js/index.js
+++ src/js/index.js
@@ -167,6 +167,7 @@ const atualizarAdministrador = () => {
       },
     })
 };
+
 $(document).ready(function () {
   $("#documento").mask("000.000.000-00");
   $("#telefone").mask("(00) 00000-0000");
